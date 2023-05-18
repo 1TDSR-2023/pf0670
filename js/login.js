1431991e1f7c204808f8c99d96aa86d05bf74771
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2023-05-17 23:26:56 -0300

    ValidaLogin evt / Valida Lista Obj[ ] Redirect

--- js/login.js
+++ js/login.js
@@ -16,12 +16,11 @@ inputEmail.addEventListener("keyup", ()=>{
     
 });
 
+const inputSenha = document.querySelector("#idPass");
 const eyePass = document.querySelector(".fa");
-
 eyePass.addEventListener("click", ()=>{
 
-    const inputSenha = document.querySelector("#idPass");
-
+console.log("VALOR : " + inputSenha.value);
     if(inputSenha.getAttribute("type") == "password"){
         inputSenha.setAttribute("type","text");
         eyePass.setAttribute("class","fa fa-eye-slash");
@@ -30,4 +29,109 @@ eyePass.addEventListener("click", ()=>{
         eyePass.setAttribute("class","fa fa-eye");
     }
 
+});
+
+
+//OBJETO
+// const usuario1 = {
+//     nomeCompleto : "Lucas das Couves",
+//     emailUsuario : "lucas@email.com",
+//     senhaUsuario : "123456"
+// }
+
+// const usuario2 = {
+//     nomeCompleto : "Juan Pelego",
+//     emailUsuario : "juan@email.com",
+//     senhaUsuario : "123456"
+// }
+// let listaDeUsuarios = [];
+// listaDeUsuarios.push(usuario1);
+// listaDeUsuarios.push(usuario2);
+
+//LISTA DE USUÁRIOS
+let listaDeUsuarios = [
+    {
+        nomeCompleto : "Lucas das Couves",
+        emailUsuario : "lucas@email.com",
+        senhaUsuario : "123456"
+    }
+    ,
+    {
+        nomeCompleto : "Juan Pelego",
+        emailUsuario : "juan@email.com",
+        senhaUsuario : "123456"
+    }
+    ,
+    {
+        nomeCompleto : "Caio Tulipa",
+        emailUsuario : "tulipa@email.com",
+        senhaUsuario : "123456"
+    }
+    ,
+    {
+        nomeCompleto : "Derick Gouvea",
+        emailUsuario : "dede@email.com",
+        senhaUsuario : "123456"
+    }
+    ,
+    {
+        nomeCompleto : "Matheuzim Danado",
+        emailUsuario : "mateu@email.com",
+        senhaUsuario : "123456"
+    }
+    ,
+    {
+        nomeCompleto : "Leunardo Camacho",
+        emailUsuario : "leleu@email.com",
+        senhaUsuario : "123456"
+    }
+];
+
+const botaoSubmit = document.querySelector("#btnSubmit");
+
+botaoSubmit.addEventListener("click", ()=>{
+
+
+    //Criando um Objeto de Validação
+    let userValidado = {
+        emailUsuarioValidado : inputEmail.value,
+        senhaUsuarioValidado : inputSenha.value
+    }
+
+    //Criando uma iteração com a lista de objetos para
+    // realizar a validação com o usuarioValidado
+   
+try{
+
+    listaDeUsuarios.forEach((usuario)=>{
+        //Realizando a validação
+        //Comparando cada objeto da lista com o objeto montado com os dados que vieram do formulário.
+        if(userValidado.emailUsuarioValidado == usuario.emailUsuario && userValidado.senhaUsuarioValidado == usuario.senhaUsuario){
+            userValidado = usuario;
+            throw "VALIDADO";
+        }
+    });
+
+    throw "SENHA OU NOME DE USUÁRIO INVÁLIDOS!";
+
+}catch(msg){
+
+    const msgStatus = document.querySelector("#msgStatus");
+
+    if(msg == "VALIDADO"){
+        msgStatus.setAttribute("style","color:#00ff00");
+        msgStatus.innerHTML = `<span><strong>Usuário: ${userValidado.nomeCompleto} Login efetuado com Sucesso!</strong></span>`;
+
+        //Redirect
+        setTimeout(()=>{
+            window.location.href = "../home.html";
+        }, 3000);
+
+
+    }else{
+        msgStatus.setAttribute("style","color:#ff0000");
+        msgStatus.innerHTML = "<span><strong>Nome de usuário ou senha inválidos...</strong></span>";
+    }
+}
+   
 });
